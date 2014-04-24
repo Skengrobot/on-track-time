16d1661b696ebd2ea38101175c92e8567be59ef3
Rudi Plesch <rudi.plesch@gmail.com>
2014-04-24 00:27:08 -0700

    Moved table drawing to a funtion

--- table-maker.py
+++ table-maker.py
@@ -3,7 +3,46 @@
 from swim import time_percent
 from swim import swim_parse
 
-conversion_percent = 3
+def make_table(conversion_percent):
+    # Latex formatting for tables
+    f.write("""\n\\begin{table}\n\centering\n\caption{\\textbf{Times """)
+    f.write(str(conversion_percent))
+    f.write(' percent off SNC GOLD on track times}}\n\\begin{tabular}{ |c||c|c|c|c|c|c|c|c|c|c| }\n\hline\n& \multicolumn{5}{|c|}{Female} & \multicolumn{5}{|c|}{Male}')
+    f.write(' \\')
+    f.write('\\')
+    f.write('\n')
+    f.write('\hline\n')
+    for i in range(len(f_ages)):
+        f.write(' & ')
+        f.write(f_ages[i])
+    for i in range(len(m_ages)):
+        f.write(' & ')
+        f.write(m_ages[i])
+    f.write('\\')
+    f.write('\\')
+    f.write('\hline\n')
+
+    # Table entries
+    for event_key in data:
+        f.write(event_key)
+        for age_key in range(len(f_ages)):
+            f.write(' & ')
+            standard = data[event_key]['female'][f_ages[age_key]]
+            conversion = time_percent(standard,conversion_percent)
+            f.write(conversion)
+        for age_key in range(len(m_ages)):
+            f.write(' & ')
+            standard = data[event_key]['male'][m_ages[age_key]]
+            conversion = time_percent(standard,conversion_percent)
+            f.write(conversion)
+        f.write(""" \\""")
+        f.write("""\\""")
+        f.write('\n')
+
+    f.write('\hline\n\end{tabular}\n\end{table}\n\n')
+
+
+
 
 data = swim_parse()
 m_ages = ['18', '17', '16', '15', '14']
@@ -25,53 +64,11 @@ f.write("""
 \\begin{document}
 """)
 
-# Latex formatting for tables
-f.write("""
-\\begin{table}
-\centering
-\caption{\\textbf{Times """)
-f.write(str(conversion_percent))
-f.write(""" percent off SNC GOLD on track times}}
-\\begin{tabular}{ |c|c|c|c|c|c|c|c|c|c|c| }
-\hline
-& \multicolumn{5}{|c|}{Female} & \multicolumn{5}{|c|}{Male}""")
-f.write(""" \\""")
-f.write("""\\
-""")
-f.write("""
-\hline
-""")
-for i in range(len(f_ages)):
-    f.write(' & ')
-    f.write(f_ages[i])
-for i in range(len(m_ages)):
-    f.write(' & ')
-    f.write(m_ages[i])
-f.write(""" \\""")
-f.write("""\\
-""")
-
-# Table entries
-for event_key in data:
-    f.write(event_key)
-    for age_key in range(len(f_ages)):
-        f.write(' & ')
-        standard = data[event_key]['female'][f_ages[age_key]]
-        conversion = time_percent(standard,conversion_percent)
-        f.write(conversion)
-    for age_key in range(len(m_ages)):
-        f.write(' & ')
-        standard = data[event_key]['male'][m_ages[age_key]]
-        conversion = time_percent(standard,conversion_percent)
-        f.write(conversion)
-    f.write(""" \\""")
-    f.write("""\\""")
-    f.write('\n')
+make_table(2)
+make_table(3)
+make_table(5)
 
 f.write("""
-\hline
-\end{tabular}
-\end{table}
 \end{document}""")
 f.truncate()
 f.close()
